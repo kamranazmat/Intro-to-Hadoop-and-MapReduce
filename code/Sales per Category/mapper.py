1b59302b307b93e6c5edac9f51fe44ef917b3b14
kamranazmat <azmat.kamran@gmail.com>
2017-02-10 16:32:38 -0500

    Sales per Category

---
+++ code/Sales per Category/mapper.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+# Format of each line is:
+# date\ttime\tstore name\titem description\tcost\tmethod of payment
+#
+# We want elements 2 (item type) and 4 (cost)
+# We need to write them out to standard output, separated by a tab
+# Kamran Azmat
+
+import sys
+
+for line in sys.stdin:
+    data = line.strip().split("\t")
+    if len(data) != 6:
+	continue
+    else:
+        date, time, store, item, cost, payment = data
+        print "{0}\t{1}".format(item, cost)
+
